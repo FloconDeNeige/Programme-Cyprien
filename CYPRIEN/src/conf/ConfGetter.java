ee0ec93d5b27c5447ac87f11d78f217c29aded57
Bruno LEIBER <satanas.linux@gmail.com>
2013-01-16 15:39:06 +0100

    firstConfVersion

--- CYPRIEN/src/conf/ConfGetter.java
+++ CYPRIEN/src/conf/ConfGetter.java
@@ -4,7 +4,15 @@
  */
 package conf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  *
@@ -12,12 +20,29 @@ import java.nio.file.Path;
  */
 public class ConfGetter {
     
-    public Path searchDirectory() {
-        return null;
+    private Properties prop = new Properties();
+    private Path path = FileSystems.getDefault().getPath(null, "prop");
+    
+    public Path searchDirectory() throws IOException {
+        try(BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            prop.load(reader);
+            String result = prop.getProperty("searchDirectory");
+            return Paths.get(result);
+        } catch (IOException ex) {
+            System.out.println("IOException occured!");
+            throw new RuntimeException();
+        }
     }
     
     public Path saveDirectory() {
-        return null;
+        try(BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            prop.load(reader);
+            String result = prop.getProperty("saveDirectory");
+            return Paths.get(result);
+        } catch (IOException ex) {
+            System.out.println("IOException occured!");
+            throw new RuntimeException();
+        }
     }
     
 }
--- CYPRIEN/src/conf/ConfSetter.java
+++ CYPRIEN/src/conf/ConfSetter.java
@@ -4,10 +4,52 @@
  */
 package conf;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 /**
  *
  * @author satanael
  */
 public class ConfSetter {
-    
+
+    private Properties prop = new Properties();
+    private Path path = FileSystems.getDefault().getPath(null, "prop");
+
+    public void searchDirectory(String dir) throws IOException {
+        try {
+            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+            prop.load(reader);
+            reader.close();
+            prop.setProperty("searchDirectory", dir);
+            BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
+            prop.store(writer, null);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("IOException occured!");
+            throw new RuntimeException();
+        }
+    }
+
+    public void saveDirectory(String dir) {
+        try {
+            BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+            prop.load(reader);
+            reader.close();
+            prop.setProperty("saveDirectory", dir);
+            BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
+            prop.store(writer, null);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("IOException occured!");
+            throw new RuntimeException();
+        }
+    }
 }
